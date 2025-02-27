ad4e969faae04c06304024c08094f00e3cfbe1ce
Ankit Dubey <106301985+ankitdevlop@users.noreply.github.com>
2025-02-28 05:05:01 +0530

    fix

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const connectDB = require("./utils/database");
 const routes = require("./routes");
 
 const app = express();
 const PORT = 4000;
 
-// Connect to database
 connectDB();
 
-// Middleware
 app.use(cors());
-app.use(express.json({ limit: "100mb" })); // Adjusted limit
-app.use(express.urlencoded({ extended: true, limit: "100mb" }));
+app.use(express.json({ limit: "1000mb" }))
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ extended: true, parameterLimit: 100000, limit: "100mb" }));
 
-// API Routes
-app.use("/api", routes);
 
-// Start Server
+
+const router = express.Router();
+app.use("/api", router);
+routes(router);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
